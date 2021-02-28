9d0b94f4b4081944359d0d90bcb828e2f64b6695
Meera <meera.patel2984@gmail.com>
2021-02-28 17:24:55 +0000

    Fix: get request no longer exposes DB id

--- controller/person.controller.js
+++ controller/person.controller.js
@@ -2,8 +2,8 @@ const Person = require('../model/person.model');
 
 async function getRandomPerson (ctx) {
   try {
-    // get one Person documment  randomly.
-    const [person] = await Person.aggregate().sample(1);
+    // get one Person documment randomly. Projection removes db id, so it's not exposed.
+    const [person] = await Person.aggregate().sample(1).project('-_id');
     if (person) ctx.body = person;
     else ctx.status = 500;
 
